c7d45cde0796b2aa824049781fafa281e1f5386d
Jorge Gomez <jandresgomez@Jorges-MacBook-Pro-2.local>
2018-04-15 23:12:26 -0500

    More modifications for Heroku deployment

--- py3/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jandresgomez/OneDrive/Academical/201810/PMC/LGSConsultoresAPP/py3/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
