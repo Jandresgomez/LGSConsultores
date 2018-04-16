4efccf45758526d0782cd8ddcb558edfd070de4b
Jorge Gomez <jandresgomez@Jorges-MacBook-Pro-2.local>
2018-04-16 01:16:37 -0500

    Test index

--- website/website/views.py
+++ website/website/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to the main website index.")
+    return HttpResponse("Welcome to the main website index. Alita es una puta")
