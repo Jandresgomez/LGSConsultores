0bd078a51830852ecb165f2efeb4cb0804c9e753
Jorge Gomez <jandresgomez@Jorges-MacBook-Pro-2.local>
2018-04-16 02:25:01 -0500

    index view rewrite

--- website/website/views.py
+++ website/website/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return render(request, 'index.html', {})
+    return HttpResponse("Welcome to the main website index.")
